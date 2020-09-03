2c0745d243234b21f721f05bd3af0605fb526f4e
jiawei <zaapzungsyugei@gmail.com>
2020-09-03 17:16:26 +0200

    2020-09-03

--- script/test.py
+++ script/test.py
@@ -7,15 +7,20 @@ testString = "Damn1988aa2345Baa"
 # "?" at the end renders non-greedy matching
 print(re.sub("(?<=\d{4})[A-Za-z]{1,2}?", "punish", testString))
 
-# Test dataframe in pandas #
+# Test dataframe in pandas
 concepts=pd.read_csv("log/concepts.csv")
-keylist=["methylation","sequence conservation","frameshift indels"]
+keylist=["methylation","sequence conservation","PTV","NMD","NMD failure","dN/dS","biallelic","MCC"]
 
-# use list() for conversion #
+# use list() for conversion
 result1=list(concepts['explanation'].loc[concepts['word']=="missense"])
 print("missense",result1)
 
-# A more generic solution to deal with problems in widths of printing #
-pd.options.display.max_colwidth = 200
+# A more generic solution to deal with problems in widths of printing
+pd.set_option('display.max_colwidth', None)
+# Or to set a specific maximum
+# pd.options.display.max_colwidth = 150 #
 result2=concepts[['word','explanation']].loc[concepts['word'].isin(keylist)]
 print(result2)
+
+# run the following bash statement to have a better view at terminal
+# ./script/test.py|sed -r 's/\s{2,}/\t/g'|sed -r 's/^[0-9]+//g' #
