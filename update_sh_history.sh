b1218206ac8d504dd17f926c2646eba9ba0f06d2
jiawei <zaapzungsyugei@gmail.com>
2020-09-04 12:23:21 +0200

    2020-09-04

--- update_sh_history.sh
+++ update_sh_history.sh
@@ -12,7 +12,7 @@ if [[ ! "$sh_linecount" == "$sh_last_linecount" ]];then
   cp /home/jiawei/.bash_history ./log/bash_history_jw
   git add ./log/bash_history_jw
   git commit -m "automatically update my ubuntu bash history at office"
-  git push
+  git push origin master
   echo $sh_linecount > /home/jiawei/.count_last_bash_history
 fi
 unset sh_last_linecount
