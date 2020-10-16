9321760aa6a4d2246de39ac9392fcdca2ca5a43c
jiaweizhao <zaapzungsyugei@gmail.com>
2020-10-16 08:45:33 +0200

    add 3dmol demo

---
+++ script/3dmol_jsmol_demo/jq_dom.js
@@ -0,0 +1,11 @@
+// this .js file uses jquery and DOM
+$("#pdb-submit").click(function() {
+  let pdb_code = $("#pdb-input").value;
+  let element = $('#container-01');
+  let config = { backgroundColor: 'orange' };
+  let viewer = $3Dmol.createViewer( element, config );
+  viewer.addSphere({ center: {x:0, y:0, z:0}, radius: 10.0, color: 'green' });
+  viewer.zoomTo();
+  viewer.render();
+  viewer.zoom(0.8, 2000);
+})
