c728cf63b036a7dd819faf61e5e11c35f40ef968
jiaweizhao <zaapzungsyugei@gmail.com>
2020-10-17 21:28:22 +0200

    implement client-side 3dmol query

--- script/3dmol_jsmol_demo/src/jq_dom.js
+++ script/3dmol_jsmol_demo/src/jq_dom.js
@@ -1,11 +1,35 @@
 // this .js file uses jquery and DOM
-$("#pdb-clear").click(function() {
-  let pdb_code = $("#pdb-input").value;
+// it's used as a middleware by "../express_srv.js"
+$(".mol-container").hide();
+
+$("#pdb-submit").click(() => {
+  $(".mol-container").show();
+  let pdb_code = $("#pdb-input").val();
   let element = $('#container-01');
-  let config = { backgroundColor: 'orange' };
-  let viewer = $3Dmol.createViewer( element, config );
-  viewer.addSphere({ center: {x:0, y:0, z:0}, radius: 10.0, color: 'green' });
-  viewer.zoomTo();
-  viewer.render();
+  let config = {
+    backgroundColor: 0xfffffff
+  };
+  let viewer = $3Dmol.createViewer(element, config);
+  $3Dmol.download('pdb:' + pdb_code, viewer, {
+    onemol: true,
+    multimodel: true
+  }, (m) => {
+    m.setStyle({
+      'cartoon': {
+        colorscheme: {
+          prop: 'ss',
+          map: $3Dmol.ssColors.Jmol
+        }
+      }
+    });
+    viewer.zoomTo();
+    viewer.render();
+  });
+  viewer.setBackgroundColor('lightgrey');
   viewer.zoom(0.8, 2000);
-})
+});
+
+$("#pdb-clear").click(() => {
+  $("#pdb-input").val('');
+  $(".mol-container").hide();
+});
