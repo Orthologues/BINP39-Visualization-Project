7020eb68df21f678f7cb4fc172625880fb92e3a1
jiaweizhao <zaapzungsyugei@gmail.com>
2020-11-19 22:12:39 +0100

    add jsmol visualization

--- script/3dmol_jsmol_demo/src/jq_dom.js
+++ script/3dmol_jsmol_demo/src/jq_dom.js
@@ -5,12 +5,14 @@ $(".mol-container").hide();
 $("#pdb-submit").click(() => {
   $(".mol-container").show();
   let pdb_code = $("#pdb-input").val();
+
+  // 3dmol
   let element = $('#container-01');
   let config = {
     backgroundColor: 0xfffffff
   };
   let viewer = $3Dmol.createViewer(element, config);
-  $3Dmol.download('pdb:' + pdb_code, viewer, {
+  $3Dmol.download(`pdb:${pdb_code}`, viewer, {
     onemol: true,
     multimodel: true
   }, (m) => {
@@ -26,7 +28,21 @@ $("#pdb-submit").click(() => {
     viewer.render();
   });
   viewer.setBackgroundColor('lightgrey');
-  viewer.zoom(0.8, 2000);
+  viewer.zoom(1, 2000);
+
+  // jsmol
+  let myJmol = 'myJmol';
+  let JmolInfo = {
+    width: '100%',
+    height: '100%',
+    color: '#E2F4F4',
+    j2sPath: '/assets/JSmol/j2s',
+    serverURL: '/assets/JSmol/php/jsmol.php',
+    script: `set zoomlarge false; set antialiasDisplay; load =${pdb_code}`,
+    use: 'html5'
+  }
+  $('#container-02').html(Jmol.getAppletHtml(myJmol, JmolInfo));
+
 });
 
 $("#pdb-clear").click(() => {
