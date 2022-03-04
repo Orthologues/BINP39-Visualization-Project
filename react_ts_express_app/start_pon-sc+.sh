9e438c585b4e4e566990aeb4972cce18926002d6
Jiawei Zhao <61454179+Orthologues@users.noreply.github.com>
2022-03-04 15:59:02 +0100

    Update start_pon-sc+.sh

--- react_ts_express_app/start_pon-sc+.sh
+++ react_ts_express_app/start_pon-sc+.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
 pm2 start /home/ji8842zh-s/PON-SC+/nodejs-backend/pon_scp_express_srv.js --name pon-scp
-cd /home/ji8842zh-s/PON-SC+/react-scripts-frontend/ && export PORT=3011 && pm2 start /home/ji8842zh-s/PON-SC+/react-scripts-frontend/node_modules/react-scripts/scripts/start.js --name view-scp
+pm2 --name view-scp start "export PORT=4998 && yarn --cwd /home/ji8842zh-s/PON-SC+/react-scripts-frontend/ start"
